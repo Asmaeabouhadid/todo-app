a5798b2ed9b6a9a8e73cc8aad4c947ec22a5fdf9
asmae <121059961+Asmaeabouhadid@users.noreply.github.com>
2024-05-22 10:37:47 +0100

    Delete app directory

--- app/(tabs)/profile/_layout.js
+++
@@ -1,9 +0,0 @@
-import {stack} from "expo-router";
-
-export default function Layout(){
-    return(
-        <Stack screenOptions={{HeaderShown:false}}>
-            <Stack.Screen name="index"/>
-        </Stack>
-    )
-}
